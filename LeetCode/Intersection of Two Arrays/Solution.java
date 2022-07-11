88a0b1d78e0f1f6077771ffdaea621c578fb7e7f
Shubham Tongaria <stongaria45@gmail.com>
2022-07-11 15:27:50 +0530

    leetcodeday3

---
+++ LeetCode/Intersection of Two Arrays/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        
+        int i=0, j=0, k=0;
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] < nums2[j]) i++;
+            else if(nums1[i] > nums2[j]) j++;
+            else {
+                nums1[k++] = nums1[i];
+                i++;
+                j++;
+            }
+        }
+        int res[] = new int[k];
+        for(i=0; i<k; i++)  res[i] = nums1[i];
+        return res;
+        }
+}
